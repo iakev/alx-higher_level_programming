f7dc260686623fda15eae84d67e704d9fa0aaf7f
iakev <kirimikmwongera@gmail.com>
2022-09-19 19:25:07 +0000

    Added a pyython script that writes to the stderr stream

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-stderr_fileno = sys.stderr
-stderr_fileno.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-exit (1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
