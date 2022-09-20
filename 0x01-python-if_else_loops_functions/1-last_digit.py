8319d18a1626288a7644f1f2a11b1f91a7256b7e
iakev <kirimikmwongera@gmail.com>
2022-09-20 15:52:48 +0000

    modified to get rid of variables

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-    if number % 10 > 5:
-        print(f"Last digit of {number:d} is {number % 10:d} and is greater than 5")
-    elif number % 10 == 0:
-        print(f"Last digit of {number:d} is {number % 10:d} and is 0")
-    elif number % 10 < 6 and number % 10 != 0:
-        print(f"Last digit of {number:d} is {number % 10:d}"\
-              f" and is less than 6 and not 0")
+if number < 0:
+    reminder = number % -10
 else:
-    if (-1 * number) % 10 == 0:
-        print(f"Last digit of {number:d} is {-1 * number % 10:d} and is 0")
-    else:
-        print(f"Last digit of {number:d} is -{-1 * number % 10:d}"\
-              f" and is less than 6 and not 0")
+    reminder = number % 10
+print(f"Last digit of', {number}, 'is', {reminder}", end=' ')
+if reminder > 5:
+    print('and is greater than 5')
+elif reminder == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
