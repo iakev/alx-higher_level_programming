94b5b0e3da9f566b698126c51ed58c5db39fe3bd
iakev <kirimikmwongera@gmail.com>
2022-09-20 16:04:25 +0000

    Modified to print the correct message

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number < 0:
     reminder = number % -10
 else:
     reminder = number % 10
-print(f"Last digit of {number}, is {reminder}", end=' ')
+print(f"Last digit of {number} is {reminder}", end=' ')
 if reminder > 5:
     print(f"and is greater than 5")
 elif reminder == 0:
