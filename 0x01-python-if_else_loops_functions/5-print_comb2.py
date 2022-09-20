29aae698b750bb582f06b8838e0de027025e527a
iakev <kirimikmwongera@gmail.com>
2022-09-20 16:09:28 +0000

    Modified to remove variables

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i < 10:
-        print(f"0{i}",end=", ")
-    else:
-        print(f"{i}",end=", " if i != 99 else "\n")
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
