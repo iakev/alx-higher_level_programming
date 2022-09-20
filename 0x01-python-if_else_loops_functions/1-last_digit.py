339bc7c0fcf562356ba8ce2c031b31f698663352
iakev <kirimikmwongera@gmail.com>
2022-09-20 16:02:50 +0000

    Added a python script to deal with combining for loops and if statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ if number < 0:
     reminder = number % -10
 else:
     reminder = number % 10
-print(f"Last digit of', {number}, 'is', {reminder}", end=' ')
+print(f"Last digit of {number}, is {reminder}", end=' ')
 if reminder > 5:
-    print('and is greater than 5')
+    print(f"and is greater than 5")
 elif reminder == 0:
-    print('and is 0')
+    print(f"and is 0")
 else:
-    print('and is less than 6 and not 0')
+    print(f"and is less than 6 and not 0")
