f691bea1de3eb2d5c1cb18d978680a4c1cf3e57e
iakev <kirimikmwongera@gmail.com>
2022-09-26 09:35:53 +0000

    Made an addition for when both tuples are empty

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a == ():
-        return tuple_b
-    if tuple_b == ():
-        return tuple_a
-    if len(tuple_a) == 1:
-        tuple_a = (tuple_a + (0,))
-    if len(tuple_b) == 1:
-        tuple_b = (tuple_b + (0,))
+    if tuple_a or tuple_b:
+        if tuple_a == ():
+            return tuple_b
+        if tuple_b == ():
+            return tuple_a
+        if len(tuple_a) == 1:
+            tuple_a = (tuple_a + (0,))
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b + (0,))
 
-    res = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return res
+        res = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+        return res
