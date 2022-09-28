949f7256af843503b7f811eeb6cbf74f4f6daed2
iakev <kirimikmwongera@gmail.com>
2022-09-28 16:43:08 +0000

    made corrections to deal with None and non string types

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
     res = 0
-    if roman_string == None:
+    if type(roman_string) != str:
         return res
     i = len(roman_string) - 1
     while i >= 0:
