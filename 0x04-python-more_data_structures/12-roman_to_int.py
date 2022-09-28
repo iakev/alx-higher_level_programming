61b64467858dd592b208db950e8dab7ac6fcd71a
iakev <kirimikmwongera@gmail.com>
2022-09-28 16:35:15 +0000

    Added a script to turn roman numerals string into intergers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pthon3
+
+def roman_to_int(roman_string):
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
+    res = 0
+    i = len(roman_string) - 1
+    while i >= 0:
+        if roman_string[i] in roman_dict:
+            if (roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]
+                    and i != 0):
+                res += (roman_dict[roman_string[i]] -
+                        roman_dict[roman_string[i - 1]])
+                i -= 1
+            else:
+                res += roman_dict[roman_string[i]]
+        else:
+            return res
+        i -= 1
+    return res
