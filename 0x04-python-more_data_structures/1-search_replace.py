627cc699c79ed15c1928c7b3e807f978c1e1d4a2
iakev <kirimikmwongera@gmail.com>
2022-09-28 10:16:01 +0000

    Commit to work on map and lambda functions

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list == []:
+        return []
+    new_list = list(map(lambda x: x if x != search else replace, my_list))
+    return new_list
