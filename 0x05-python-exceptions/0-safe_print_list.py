bb151efdb98a844175276ebb5da4595a3acacde4
iakev <kirimikmwongera@gmail.com>
2022-10-11 09:23:53 +0000

    modified for pycodestyle linter

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[],x=0):
+def safe_print_list(my_list=[], x=0):
     """
     function that prints x elements of a list 'my_list'
     """
     try:
-        for i in range(0,x):
+        for i in range(0, x):
             print(my_list[i], end="")
     except IndexError:
         return i
