f94cc7d102690ad31a8ebdabbc4ded2826d9089a
iakev <kirimikmwongera@gmail.com>
2022-10-11 10:46:10 +0000

    worked with a counter variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,13 @@ def safe_print_list(my_list=[], x=0):
     """
     function that prints x elements of a list 'my_list'
     """
+    counter = 0
     try:
         for i in range(0, x):
             print(my_list[i], end="")
+            counter += 1
     except IndexError:
-        return i
-    else:
-        return i + 1
+        pass
     finally:
         print()
+        return counter
