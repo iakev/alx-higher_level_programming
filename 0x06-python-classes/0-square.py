59d38110b421b6e196b0fd6a1b202a9fb664fed8
iakev <kirimikmwongera@gmail.com>
2022-10-12 12:23:19 +0000

    corrections to deal with linter

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,8 +2,11 @@
 """
 A module that highlights how to create an empty square class
 """
+
+
 class Square:
     """
     A class that abstracts the square shape object currently empty
     """
+
     pass
