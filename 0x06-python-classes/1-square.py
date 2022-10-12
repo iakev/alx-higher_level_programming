07477891dc90ded782b06bfbc1120907650443b4
iakev <kirimikmwongera@gmail.com>
2022-10-12 12:29:39 +0000

    added a function to meet task1 requirements

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module that highlights how to create an empty square class
+"""
+
+
+class Square:
+    """
+    A class that abstracts the square shape object currently empty
+    """
+
+    def __init__(self, size):
+        self.__size = size
