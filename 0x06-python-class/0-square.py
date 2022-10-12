f8cc40c7d1b931a0781b171e514eec16a026fdf7
iakev <kirimikmwongera@gmail.com>
2022-10-12 11:48:14 +0000

    First commit and task 0

---
+++ 0x06-python-class/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+A module that highlights how to create an empty square class
+"""
+class Square:
+    """
+    A class that abstracts the square shape object currently empty
+    """
+    pass
