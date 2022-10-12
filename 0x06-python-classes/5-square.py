f0ace0d59bf38850a1e8296cef68d65be71a8050
iakev <kirimikmwongera@gmail.com>
2022-10-12 13:36:41 +0000

    made corrections to deal with when size is zer

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -34,7 +34,9 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
+        if self.__size == 0:
+            print()
         for i in range(0, self.__size):
             for j in range(0, self.__size):
-                print("#",end="")
+                print("#", end="")
             print()
