0f779ff0bdcf4799ac01bafa28717dbf161a508b
iakev <kirimikmwongera@gmail.com>
2022-10-12 14:32:53 +0000

    made some corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,8 +73,5 @@ class Square:
         for m in range(self.__position[1]):
             print()
         for i in range(0, self.__size):
-            for k in range(0, self.__position[0]):
-                print(" ", end="")
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for l in range(self.__size)]))
