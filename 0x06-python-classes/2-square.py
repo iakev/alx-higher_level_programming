eb3a664d9dddb97cce587502e6ffea289cf41b62
iakev <kirimikmwongera@gmail.com>
2022-10-12 12:48:53 +0000

    Added a function to deal with task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module that highlights how to create a square class with initialization
+"""
+
+
+class Square:
+    """
+    A class that abstracts the square shape object currently has a size
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
