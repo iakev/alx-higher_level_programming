adc4014282f0a1a05a064087d6e8f0f108c09b2a
iakev <kirimikmwongera@gmail.com>
2022-10-17 13:13:55 +0000

    added a module that defines a class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that defines a class called
+Rectangle that defines all about rectangles
+"""
+
+
+class Rectangle:
+    """
+    Actual definition of the class
+    """
+    pass
