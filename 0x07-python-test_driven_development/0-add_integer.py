f56843a7d1bca9a42530df5ad394a407e7450e79
iakev <kirimikmwongera@gmail.com>
2022-10-17 10:44:40 +0000

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module that defines addition functions for integers and float types
+"""
+
+def add_integer(a, b=98):
+    """
+    A function that adds two integers a and b if floats first converts them to
+    integers
+    """
+    if type(a) not in [float ,int]:
+        raise TypeError("a must be integer")
+    elif type(b) not in [float, int]:
+        raise TypeError("b must be integer")
+    else:
+        return int(a) + int(b)
