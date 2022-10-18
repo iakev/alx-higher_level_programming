63fe8dcc7dfcb03b6fbc6ca2c4f9ac80c251f615
iakev <kirimikmwongera@gmail.com>
2022-10-18 14:14:27 +0000

    modified to deal with linter

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,12 +3,13 @@
 A module that defines addition functions for integers and float types
 """
 
+
 def add_integer(a, b=98):
     """
     A function that adds two integers a and b if floats first converts them to
     integers
     """
-    if type(a) not in [float ,int]:
+    if type(a) not in [float, int]:
         raise TypeError("a must be an integer")
     elif type(b) not in [float, int]:
         raise TypeError("b must be an integer")
