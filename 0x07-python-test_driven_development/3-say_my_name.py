fbc76a1dd46f26ea774ea4edaf6efe13b536bec3
iakev <kirimikmwongera@gmail.com>
2022-10-21 13:22:13 +0000

    changed output when onlly first_name is supllied

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,7 +16,4 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) != str:
         raise TypeError("last_name must be a string")
     else:
-        if last_name != "":
-            print(f"My name is {first_name} {last_name}")
-        else:
-            print(f"My name is {first_name}")
+        print(f"My name is {first_name} {last_name}")
