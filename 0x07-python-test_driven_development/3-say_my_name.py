a3dee2d1ef6b7543c47da406d4ec6119476e1842
iakev <kirimikmwongera@gmail.com>
2022-10-21 13:19:27 +0000

    added a function that prints first and last name with accompanying test suite

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module that defines a function that prints first name
+and last name as passed to it
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    A function printing a sentence conataining
+    first and last name
+    """
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    else:
+        if last_name != "":
+            print(f"My name is {first_name} {last_name}")
+        else:
+            print(f"My name is {first_name}")
