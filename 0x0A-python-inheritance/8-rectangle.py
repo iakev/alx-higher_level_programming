2663e134981cac760a1938700af80ff2f4be58ac
iakev <kirimikmwongera@gmail.com>
2022-10-24 16:02:46 +0000

    made some corrections to unbundle width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle(BaseGeometry):
         Rectangle's initializer function
         """
 
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
