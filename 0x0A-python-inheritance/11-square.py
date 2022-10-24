63145977be2f67e81d014d1e36f990bbd517ae3b
iakev <kirimikmwongera@gmail.com>
2022-10-24 17:53:25 +0000

    added a string representation customised for the square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A module that defines a square class that
+inherits from the previously defined rectangle class
+in '9-rectangle.py'
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that defines a square class inheriting
+    from Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Initilaizing square with an equal side 'size'
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        compute area of square
+        """
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Reresentation of square class
+        """
+        return super().__str__()
