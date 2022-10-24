4e5ee2469b5f92663b681acd70334738ebc5156b
iakev <kirimikmwongera@gmail.com>
2022-10-24 16:06:33 +0000

    made some corrections to unbundle width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -28,8 +28,6 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        else:
-            pass
 
 
 class Rectangle(BaseGeometry):
