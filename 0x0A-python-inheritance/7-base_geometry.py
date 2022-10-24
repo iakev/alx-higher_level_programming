083192f5ccd57a40769b4729b055ae6cd6eaced6
iakev <kirimikmwongera@gmail.com>
2022-10-24 15:41:22 +0000

    added a value validator public instance method with its test suites

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A model that defines an empty class called
+'BaseGeometry'
+"""
+
+
+class BaseGeometry:
+    """
+    A class that generically defines different
+    geometries
+    """
+
+    def area(self):
+        """
+        A method that implements the area of a geometry
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        A method that validates value
+        """
+        if type(name) != str:
+            raise TypeError("name should be a string")
+        elif type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
