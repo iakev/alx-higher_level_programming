2dc11e6ce625ee56ec7345ed4e454684d4a2401d
iakev <kirimikmwongera@gmail.com>
2022-10-25 12:57:17 +0000

    added a script that utilizes the two made functions for saving and loading from json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,13 +11,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
-    with open(filename, encoding="utf-8") as f:
-        ls = load_from_json_file(filename)
-        for i in range(1, len(sys.argv)):
-            ls.append(sys.argv[i])
-        save_to_json_file(ls, filename)
+    ls = load_from_json_file(filename)
+except:
+    ls = []
 
-except FileNotFoundError:
-    with open(filename, 'w', encoding="utf-8") as f:
-        ls = []
-        save_to_json_file(ls, filename)
+for i in range(1, len(sys.argv)):
+    ls.append(sys.argv[i])
+
+save_to_json_file(ls, filename)
