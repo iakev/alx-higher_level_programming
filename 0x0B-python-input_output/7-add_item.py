e0bbdba548cf5a916b08a641a6a29644ac95e73e
iakev <kirimikmwongera@gmail.com>
2022-10-25 13:00:38 +0000

    made linter corrections

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 try:
     ls = load_from_json_file(filename)
-except:
+except Exception as e:
     ls = []
 
 for i in range(1, len(sys.argv)):
