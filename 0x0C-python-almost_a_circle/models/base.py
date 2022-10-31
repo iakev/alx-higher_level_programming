36e24e4cb45c305f0ef40354f905871ac1a01fd3
iakev <kirimikmwongera@gmail.com>
2022-10-31 21:28:52 +0000

    added a class method to Base that serializes a list of python dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@ A module that defines a base class with
 it's private attributes and a class constructor
 """
 
+import json
+
 
 class Base:
     """
@@ -19,3 +21,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns a JSON string representation of list_dictionaries.
+        list_dictionaries is  a list of dictionaries
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
