f0ea36e8e098da7e347a8ef3ddd3f76fa365a1f9
iakev <kirimikmwongera@gmail.com>
2022-10-31 21:52:34 +0000

    added a method that saves JSON string representation of objects to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,23 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes JSON string representation of list_objs to
+        a file
+        """
+        try:
+            filename = str(type(list_objs[0]).__name__)
+            filename = filename + ".json"
+            list_dictionaries = []
+            for obj in list_objs:
+                list_dictionaries.append(obj.to_dictionary())
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(list_dictionaries))
+
+        except:
+            filename = "Rectangle.json"
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(None))
