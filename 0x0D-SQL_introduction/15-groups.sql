26c8168d4b8847f58d1ae41dd44ca2d9ab916351
iakev <kirimikmwongera@gmail.com>
2022-11-29 22:23:26 +0000

    added a script to list and group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 -- Script lists number of records with the same score in the table 'second_table'"
-SELECT DISTINCT score, COUNT(*) as number FROM second_table;
+SELECT score, COUNT(*) as number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
