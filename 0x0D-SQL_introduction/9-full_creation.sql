b86e9d25439716fc546b25294227c5726be4511a
iakev <kirimikmwongera@gmail.com>
2022-11-29 20:58:17 +0000

    A script that creates a table second_table in hbtn_0c_0 and adds multiples row

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Creates table second_table and adds multiples row
+CREATE TABLE if not exists second_table(
+       id INT,
+       name VARCHAR(256),
+       score INT
+);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10),(2, "Alex", 3),(3, "Bob", 14), (4, "George", 8);
