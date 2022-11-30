332b06ece457ccae78f2d1b40e955bc86ad56ff6
iakev <kirimikmwongera@gmail.com>
2022-11-30 17:51:55 +0000

    modified script to not select everthing but id and name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Script that lists all cities of california
 -- in hbtn_0d_usa using a subquery
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id =
       (SELECT id FROM states
       WHERE name = "California")
