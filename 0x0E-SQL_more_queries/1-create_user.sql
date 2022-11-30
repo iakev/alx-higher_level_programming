c10f8a89c0634d71fdf6112cec650307bac139ca
iakev <kirimikmwongera@gmail.com>
2022-11-30 14:03:40 +0000

    made corrections to set correct password

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates MySQL server user user_0d_1 with all priveleges on server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_2';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
