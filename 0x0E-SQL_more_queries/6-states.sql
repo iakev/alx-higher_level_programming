b302dd6f17f6cabf5fd97d6994ff99c294c4a5fe
iakev <kirimikmwongera@gmail.com>
2022-11-30 17:03:13 +0000

    added a script to create a database add a table to it with columns constrained with NOT NULL, PK, unique and auto generated

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Gracefully creates a database 'hbtn_0d_usa' and gracefully adds 'states'
+-- table with id (unique auto generated can't be null and is a pk)
+-- table also contains name which can't be Null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL
+);
