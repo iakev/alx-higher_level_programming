52ed65ec0859546b0245a126e979bc5655e4b42f
iakev <kirimikmwongera@gmail.com>
2022-11-30 21:14:49 +0000

    added a script that deals with inner join on two tables with a pk/fk relationshin

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- List all shwows containied in hbtn _0d_tvshows with one genre linked
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+     INNER JOIN tv_show_genres
+     ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
