5efcb604e23da91a51a4920c5e11535ff7cdc1d3
iakev <kirimikmwongera@gmail.com>
2022-12-01 17:59:27 +0000

    modified database, table, field to utf8mb4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,3 @@
 -- Convert DATABASE, table and field into utf8mb4 collate utf3mb4_unicode_ci
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CCOLLATE utf8mb4_unicode_ci;
