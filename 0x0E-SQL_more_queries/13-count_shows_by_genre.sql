57c0ad4998679abe6a79594c3bb5c89ec4ab06d2
iakev <kirimikmwongera@gmail.com>
2022-12-01 10:58:14 +0000

    modified script to alias first field requested as genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Counts number of shows linked to each tv show
-SELECT tv_genres.name, COUNT(*) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
      INNER JOIN tv_show_genres
      ON tv_genres.id = tv_show_genres.genre_id
