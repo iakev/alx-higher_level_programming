89119e6f6a37df33a0c0578ea51e40ea51947f7d
iakev <kirimikmwongera@gmail.com>
2022-12-01 12:45:54 +0000

    added a script that joins three tables to get entry from first table coninciding wth a condition on the third table

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- List all shows of a certain genre 'Comedy'
+-- using MULTIPLE INNER JOINS joining
+-- over 3 tables
+SELECT tv_shows.title
+FROM tv_shows
+     INNER JOIN tv_show_genres
+     ON tv_shows.id = tv_show_genres.show_id
+
+     INNER JOIN tv_genres
+     ON tv_show_genres.genre_id = tv_genres.id
+     WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
