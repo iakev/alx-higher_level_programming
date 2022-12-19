a569683ca7d323a2d5bd81adb80305b3281a1bfa
iakev <kirimikmwongera@gmail.com>
2022-12-19 22:27:25 +0000

    added a script that prints the first argument passed to it and ignores the non argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+let finalIndex = 0;
+
+process.argv.forEach((val, index) => {
+  if (index > 1) {
+    console.log(val);
+  }
+  finalIndex = index;
+});
+if (finalIndex === 1) {
+  console.log('No argument');
+}
