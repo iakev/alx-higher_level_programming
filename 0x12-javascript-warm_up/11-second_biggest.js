8b74da6855cbb117c82b01615384abed5e9e1a12
iakev <kirimikmwongera@gmail.com>
2022-12-20 10:21:58 +0000

    added a script that searched the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argvLen = process.argv.length;
+const argArr = [];
+
+if (argvLen <= 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < argvLen; i++) {
+    const argInt = parseInt(process.argv[i]);
+    argArr.push(argInt);
+  }
+  argArr.sort();
+  console.log(argArr[argArr.length - 2]);
+}
