5dbfc6b81abd15faacee8190c2ddea67fc49cb23
iakev <kirimikmwongera@gmail.com>
2022-12-20 08:51:41 +0000

    modified script to print the phrase 'My number'

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const process = require('process');
-const lenArgv = process.argv.length;
-let convertArg = parseInt(process.argv[2]);
-if (convertArg){
-  console.log(convertArg);
+const convertArg = parseInt(process.argv[2]);
+if (convertArg) {
+  console.log(`My number: ${convertArg}`);
 } else {
   console.log('Not a number');
 }
