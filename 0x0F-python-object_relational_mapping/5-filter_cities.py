c5c4fea4711d669de147088f64121e829e3964f6
iakev <kirimikmwongera@gmail.com>
2022-12-21 19:40:19 +0000

    added a script to filter cities according to their state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module that takes in state name as an argument and displays
+all cities in that state where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+hst = "localhost"
+usr = sys.argv[1]
+passwod = sys.argv[2]
+dbname = sys.argv[3]
+state = str(sys.argv[4])
+prt = 3306
+
+
+def query_db():
+    """
+    Establishes actual connection to database
+    and performs the query and prints the results
+    """
+
+    query = """SELECT cities.name
+    FROM cities
+    WHERE cities.state_id =
+    (SELECT states.id FROM
+    states WHERE states.name = %s)
+    ORDER BY cities.id"""
+    db = MySQLdb.connect(host=hst, user=usr,
+                         passwd=passwod, db=dbname, port=prt)
+    cur = db.cursor()
+    cur.execute(query, (state,))
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        for j in range(len(rows[i])):
+            print(rows[i][j], end=", " if i != len(rows) - 1 else "\n")
+
+
+if __name__ == "__main__":
+    query_db()
