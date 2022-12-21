1f25d12296c1ecbe14968db3beaf102262e87574
iakev <kirimikmwongera@gmail.com>
2022-12-21 20:06:22 +0000

    modified to ensure once code is exported it wont execute

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,19 +6,13 @@ Module to lists all cities from specified database
 import sys
 import MySQLdb
 
-hst = "localhost"
-usr = sys.argv[1]
-passwod = sys.argv[2]
-dbname = sys.argv[3]
-prt = 3306
 
-
-def query_db():
-    """
-    Establishes actual connection to database
-    and performs the query and prints the results
-    """
-
+if __name__ == "__main__":
+    hst = "localhost"
+    usr = sys.argv[1]
+    passwod = sys.argv[2]
+    dbname = sys.argv[3]
+    prt = 3306
     query = """SELECT cities.id, cities.name, states.name
     FROM cities JOIN states WHERE states.id = cities.state_id
     ORDER BY cities.id"""
@@ -29,7 +23,3 @@ def query_db():
     rows = cur.fetchall()
     for row in rows:
         print(f"{row}")
-
-
-if __name__ == "__main__":
-    query_db()
