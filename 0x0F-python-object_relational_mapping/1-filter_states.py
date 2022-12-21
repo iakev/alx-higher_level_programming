d71890e2c24fdb689c857d17f8ceac9c90a9b1a5
iakev <kirimikmwongera@gmail.com>
2022-12-21 19:50:20 +0000

    modified to ensure once code is exported it wont execute

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,19 +9,13 @@ specified database
 import sys
 import MySQLdb
 
-hst = "localhost"
-usr = sys.argv[1]
-passwod = sys.argv[2]
-dbname = sys.argv[3]
-prt = 3306
 
-
-def query_db():
-    """
-    Establishes actual connection to database
-    and performs the query and prints the results
-    """
-
+if __name__ == "__main__":
+    hst = "localhost"
+    usr = sys.argv[1]
+    passwod = sys.argv[2]
+    dbname = sys.argv[3]
+    prt = 3306
     db = MySQLdb.connect(host=hst, user=usr,
                          passwd=passwod, db=dbname, port=prt)
     cur = db.cursor()
@@ -29,7 +23,3 @@ def query_db():
     rows = cur.fetchall()
     for row in rows:
         print(f"{row}")
-
-
-if __name__ == "__main__":
-    query_db()
