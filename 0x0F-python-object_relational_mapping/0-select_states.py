3eba66c3553da4b8e8b2593177081fddb5a21602
iakev <kirimikmwongera@gmail.com>
2022-12-21 19:39:10 +0000

    modified for linter

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-
-
 """
 Module that lists all states from the database hbtn_0e_0_usa
 """
 
 import sys
 import MySQLdb
-
-hst = "localhost"
-usr = sys.argv[1]
-passwod = sys.argv[2]
-dbname = sys.argv[3]
-prt = 3306
 
 
 def query_db():
@@ -21,10 +13,15 @@ def query_db():
     and performs the query and prints the results
     """
 
+    hst = "localhost"
+    usr = sys.argv[1]
+    passwod = sys.argv[2]
+    dbname = sys.argv[3]
+    prt = 3306
     db = MySQLdb.connect(host=hst, user=usr,
                          passwd=passwod, db=dbname, port=prt)
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states ORDER BY id")
+    cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(f"{row}")
