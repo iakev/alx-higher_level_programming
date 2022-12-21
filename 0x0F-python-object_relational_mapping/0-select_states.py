b0a61207475e7b91853b06c9250ee227ff5da7de
iakev <kirimikmwongera@gmail.com>
2022-12-21 14:16:24 +0000

    modified to ensure module not executable when exported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,10 +13,19 @@ usr = sys.argv[1]
 passwod = sys.argv[2]
 dbname = sys.argv[3]
 prt = 3306
-db = MySQLdb.connect(host=hst, user=usr,
-                     passwd=passwod, db=dbname, port=prt)
-cur = db.cursor()
-cur.execute(f"SELECT * FROM states ORDER BY id")
-rows = cur.fetchall()
-for row in rows:
-    print(f"{row}")
+def query_db():
+    """
+    Establishes actual connection to database
+    and performs the query and prints the results
+    """
+
+    db = MySQLdb.connect(host=hst, user=usr,
+                         passwd=passwod, db=dbname, port=prt)
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"{row}")
+
+if __name__ == "__main__":
+    query_db()
