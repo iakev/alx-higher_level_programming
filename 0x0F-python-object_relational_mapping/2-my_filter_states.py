cd6171ee59d1b3012985cf3ad1c4023289d7dfb7
iakev <kirimikmwongera@gmail.com>
2022-12-21 19:59:25 +0000

    modified to ensure once code is exported it wont execute

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,20 +9,14 @@ values in states where name matches the argument
 import sys
 import MySQLdb
 
-hst = "localhost"
-usr = sys.argv[1]
-passwod = sys.argv[2]
-dbname = sys.argv[3]
-search = str(sys.argv[4])
-prt = 3306
 
-
-def query_db():
-    """
-    Establishes actual connection to database
-    and performs the query and prints the results
-    """
-
+if __name__ == "__main__":
+    hst = "localhost"
+    usr = sys.argv[1]
+    passwod = sys.argv[2]
+    dbname = sys.argv[3]
+    search = str(sys.argv[4])
+    prt = 3306
     query = """SELECT * FROM states WHERE name = '{search}'"""
     db = MySQLdb.connect(host=hst, user=usr,
                          passwd=passwod, db=dbname, port=prt)
@@ -32,7 +26,3 @@ def query_db():
     rows = cur.fetchall()
     for row in rows:
         print(f"{row}")
-
-
-if __name__ == "__main__":
-    query_db()
