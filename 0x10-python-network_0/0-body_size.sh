4353367281bd2a4799e4aaffcc649b49027b8d14
iakev <kirimikmwongera@gmail.com>
2023-01-16 16:31:43 +0000

    modified shebang directive and ensure the line count is 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-# script taking in a URL, sending a request to the URl, and displaying
-# size of the body response
-curl $1 -sw -w '${size_download}\n' | grep -o '[0-9]*'
+#!/bin/bash
+# script taking in a URL, sending a request to the URl,displaying size of conte
+curl $1 -sw '${size_download}\n' | grep -o '[0-9]*'
