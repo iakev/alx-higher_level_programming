ffa3414d9c5ae36da6e172e233c06fa410ef6cf8
iakev <kirimikmwongera@gmail.com>
2023-01-16 17:11:42 +0000

    modified to pass checker

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script taking in a URL, sending a request to the URl,displaying size of conte
-curl $1 -sw '${size_download}\n' | grep -o '[0-9]*'
+curl -o - $1 -sw '${size_download}\n' | grep -o '[0-9]*'
