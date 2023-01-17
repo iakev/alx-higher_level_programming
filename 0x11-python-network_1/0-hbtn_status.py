b164472d526dc080167c3cac99225dc7b3ec4e33
iakev <kirimikmwongera@gmail.com>
2023-01-17 13:39:47 +0000

    added a script that fetches and formats response body as specified

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script fetching from supplied url and
+displaying the response body
+"""
+
+import urllib.request
+from pprint import pprint
+if __name__ == "__main__":
+    url  = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print(f"\t - type: {type(content)}")
+        print(f"\t - content: {content}")
+        print(f"\t - utf8 content: {response.msg}")
