1c11464a7d250f69ef279f38dd5db63125af28bf
iakev <kirimikmwongera@gmail.com>
2023-01-17 21:35:34 +0000

    added a script that takes in a letter and sends a POST request with that letter as aparameter and reads the JSON response dealing with errors

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script taking in a letter sending a POST request to a URL and displaying
+the resulting JSON if valid
+"""
+
+import requests
+import sys
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        letter = sys.argv[1]
+        payload = {'q': letter}
+    except IndexError:
+        payload = {'q': ""}
+    try:
+        r = requests.post(url, data=payload)
+        try:
+            json_dict = r.json()
+            if json_dict:
+                print("[{}] {}"
+                      .format(json_dict.get('id'), json_dict.get('name')))
+            else:
+                print("No result")
+        except r.exceptions.JSONDecodeError as e:
+            print("Not a valid JSON")
+    except r.raise_for_status():
+        pass
