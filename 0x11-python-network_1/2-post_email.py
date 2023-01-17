98757d000fe2ed24dde3c500d9d4b486e8dbb753
iakev <kirimikmwongera@gmail.com>
2023-01-17 17:15:02 +0000

    modified to change f python strings to format strings for checker

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        print(f"{content.decode('utf-8')}")
+        print("{}".format(content.decode('utf-8')))
