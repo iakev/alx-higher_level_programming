e8feface5550f16511a59961f1198c2d45827082
iakev <kirimikmwongera@gmail.com>
2023-01-17 17:58:05 +0000

    modified output format mode

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,5 @@ import requests
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
-    print("Body response:")
-    print("    - type: {}".format(type(r.text)))
-    print("    - content: {}".format(r.text))
+    print("Body response:\n \t - type: {}\n \t - content: {}"
+          .format(type(r.text), r.text))
