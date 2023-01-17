4b6270910f1f9b8b5d6286b81b4504b44e394cae
iakev <kirimikmwongera@gmail.com>
2023-01-17 10:04:37 +0000

    modified to silence curl output during operation

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to POST to url using curl displaying resonse body
-curl -X POST -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' $1
+curl -sX POST -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' $1
