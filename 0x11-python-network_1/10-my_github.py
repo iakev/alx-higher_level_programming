03c695e24221e03c5aed029768da53b5e5e4a937
iakev <kirimikmwongera@gmail.com>
2023-01-18 09:06:36 +0000

    modified to make authorization well to github API, and corrected pycodestyle linter

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,10 +6,13 @@ script taking my Github credentials, uses Githu API to display my id
 import requests
 import sys
 if __name__ == "__main__":
-    github_url = 'https://api.github.com/users/'
+    github_url = 'https://api.github.com/user'
     uname = sys.argv[1]
     token = sys.argv[2]
-    github_url = github_url + uname
-    r = requests.get(github_url, auth=('uname', 'token'))
+    token = "Bearer " + token
+    headers = {"Accept": "application/vnd.github+json",
+               "Authorization": token,
+               "X-Github-Api-Version": "2022-11-28"}
+    r = requests.get(github_url, headers=headers)
     json_dict = r.json()
     print("{}".format(json_dict.get('id')))
