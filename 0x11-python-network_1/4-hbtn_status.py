3f00d16b2a11c71992eeb51ceddeeaca693ad208
iakev <kirimikmwongera@gmail.com>
2023-01-18 07:56:02 +0000

    modified to get correct number of output characters

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,6 @@ import requests
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
-    print("Body response:\n\t - type: {}\n\t - content: {}"
-          .format(type(r.text), r.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- type: {}".format(r.text))
